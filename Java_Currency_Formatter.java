faead5ee246df9c7dce8ea583ca3e4cb30132f63
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-28 19:37:07 +0530

    Update Java_Currency_Formatter.java

--- Java_Currency_Formatter.java
+++ Java_Currency_Formatter.java
@@ -45,3 +45,40 @@ Each line contains the value of  formatted according to the four countries' resp
 
 
 */
+
+
+
+
+
+
+
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payement = scanner.nextDouble();
+        scanner.close();
+        
+        // Write your code here.
+
+
+
+        Locale obj = new Locale("en","IN");
+
+        
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(obj);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+
+        System.out.println("US: " + us.format(payement));
+        System.out.println("India: " + india.format(payement));
+        System.out.println("China: " + china.format(payement));
+        System.out.println("France: " + france.format(payement));
+    }
+}
+
