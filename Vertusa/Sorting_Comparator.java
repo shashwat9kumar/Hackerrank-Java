53cb37c3d6355b5df4ecf09da75168d13db736fb
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-07-02 17:29:24 +0530

    Create Sorting_Comparator.java

---
+++ Vertusa/Sorting_Comparator.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Player {
+	String name;
+	int score;
+
+	Player(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+}
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player o2, Player o1) {
+         if(o1.score < o2.score) return -1;
+        else if(o1.score > o2.score) return 1;
+        else
+        {
+            if(o1.name.compareTo(o2.name)<0) return 1;
+            else return -1;
+        }
+    
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
